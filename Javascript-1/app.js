9685e0d175787a903b953abd9899de92367a84a5
HammadShinwari33 <hammadshinwari377@gmail.com>
2023-09-17 12:00:20 +0500

    JS-Practice

---
+++ Javascript-1/app.js
@@ -0,0 +1,68 @@
+
+// ================ CHAPTER 01 ==============
+
+
+
+// ================ CHAPTER 02 ==============
+
+
+// let username;
+
+// let myName = "Your Full Name";
+
+// let message;
+// message = "Hello World"
+// alert(message);
+
+// let studentName = "Hammad";
+// let studentAge = 17;
+// let studentlearning = "Web and mobile application";
+// alert("Student Name: " + studentName + "\nStudent Age: " + studentAge + "\nlearning: " + studentlearning);
+
+// let alertMessage = "This is a single variable alert!";
+// alert(alertMessage);
+
+// let email = "hammadshinwari377@gmail.com";
+// alert("My email address is: " + email);
+
+// let book = "A smarter way to learn javascript";
+// alert("I am currently reading the book titled:\n" + book);
+
+// document.write("Yah! I can write HTML content through JavaScript.");
+
+// let specialCharacters = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(specialCharacters);
+// document.write(specialCharacters);
+
+
+// ================ CHAPTER 03 ==============
+
+
+// let age = "I am 17 year old";
+// alert(age);
+
+// var visitCount = localStorage.getItem("visitCount");
+
+// if (visitCount === null) {
+//   visitCount = 1;
+// } else {
+//   visitCount = parseInt(visitCount) + 1;
+// }
+
+// localStorage.setItem("visitCount", visitCount);
+
+// document.write("You have visited this site " + visitCount + " times.");
+
+// let birthYear = 2006;
+// document.write("Your birth year is: " + birthYear);
+
+
+// ================ CHAPTER 04 ==============
+
+
+
+// ================ CHAPTER 05 ==============
+
+
+
+// ================ CHAPTER 06 ==============
